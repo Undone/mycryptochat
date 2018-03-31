984b1cfe5555fee9e633b6e498a87fe7805b65b6
Undone <undonecoding@outlook.com>
2018-03-31 21:10:30 +0300

    Code cleanup and documentation

--- getMessages.php
+++ getMessages.php
@@ -6,64 +6,82 @@ require 'inc/functions.php';
 require 'inc/classes.php';
 require 'inc/dbmanager.php';
 
-$dbManager = new DbManager();
+$dbManager 			= new DbManager();
+$roomid 			= filter_input(INPUT_POST, "roomId");
+$dateLastNewMessage = filter_input(INPUT_POST, "dateLastGetMessages");
+$chatRoom 			= $dbManager->GetChatroom($roomid);
 
-$chatRoom = $dbManager->GetChatroom($_POST['roomId']);
-$dateLastNewMessage = $_POST['dateLastGetMessages'];
-$nbIps = $_POST['nbIps'];
-
-if(!is_null($chatRoom)) {
-    $userHash = getHashForIp();
-    $time = $_SERVER['REQUEST_TIME'];
-
-    if($chatRoom->dateEnd != 0 && $chatRoom->dateEnd <= $time) {
-        echo 'noRoom';
-        exit;
-    }
-
-    $currentUser = null;
-
-    foreach($chatRoom->users as $key => $user) {
-        if(!is_null($user)) {
-            if($user['id'] == $userHash) {
-                $currentUser = $user;
-            }
-            if(!$chatRoom->noMoreThanOneVisitor && $user['dateLastSeen'] + NB_SECONDS_USER_TO_BE_DISCONNECTED < $time) {
-                unset($chatRoom->users[$key]);
-            }
-        }
-    }
-
-    if(is_null($currentUser)) {
-        $currentUser = array();
-        $currentUser['id'] = $userHash;
-        $currentUser['dateLastSeen'] = $time;
-        array_push($chatRoom->users, $currentUser);
-    } else {
-        $currentUser['dateLastSeen'] = $time;
-    }
-
-    if($chatRoom->noMoreThanOneVisitor && count($chatRoom->users) > 2) {
-        $dbManager->DeleteChatroom($_POST['roomId']);
-        echo 'destroyed';
-        exit;
-    }
-
-    if($dateLastNewMessage < $chatRoom->dateLastNewMessage) {
-        $dbManager->UpdateChatRoomUsers($chatRoom);
-        $messages = $dbManager->GetLastMessages($chatRoom->id, NB_MESSAGES_TO_KEEP);
-        header('Content-Type: application/json');
-        echo '{ "dateLastGetMessages": ',$time,', "chatLines": ',json_encode($messages),', "nbIps": ',count($chatRoom->users),' }';
-        exit;
-    } else if ($nbIps != count($chatRoom->users)) {
-        $dbManager->UpdateChatRoomUsers($chatRoom);
-        header('Content-Type: application/json');
-        echo '{ "nbIps": ',count($chatRoom->users),' }';
-        exit;
-    } else {
-        echo 'noNew';
-        exit;
-    }
+if (!$chatRoom)
+{
+	exit;
 }
-echo 'noRoom';
-?>
+
+$userHash 	= getHashForIp();
+$time 		= time();
+
+// Check if the room has expired
+if($chatRoom->dateEnd != 0 && $chatRoom->dateEnd <= $time)
+{
+	echo 'noRoom';
+	exit;
+}
+
+$currentUser = null;
+
+// Loop over the rooms users, check if the current user is associated with the room
+foreach($chatRoom->users as $key => $user)
+{
+	if($user['id'] == $userHash)
+	{
+		$currentUser = $user;
+	}
+
+	if(!$chatRoom->noMoreThanOneVisitor && ($user['dateLastSeen'] + NB_SECONDS_USER_TO_BE_DISCONNECTED) < $time)
+	{
+		unset($chatRoom->users[$key]);
+	}
+}
+
+// If the current user isn't associated with the current room, add him
+// Otherwise update current users last seen timestamp
+if (!$currentUser)
+{
+	$currentUser = array();
+	$currentUser['id'] = $userHash;
+	$currentUser['dateLastSeen'] = $time;
+	
+	$chatRoom->addUser($currentUser);
+	$dbManager->UpdateChatRoomUsers($chatRoom);
+}
+else
+{
+	$currentUser['dateLastSeen'] = $time;
+}
+
+// If the room is only allowed to have 2 users, delete it when another user joins
+if($chatRoom->noMoreThanOneVisitor && count($chatRoom->users) > 2)
+{
+	$dbManager->DeleteChatroom($roomid);
+	exit;
+}
+
+// Save changes to the database
+$dbManager->UpdateChatRoomUsers($chatRoom);
+
+// Check if there are messages that should be sent
+if($dateLastNewMessage < $chatRoom->dateLastNewMessage)
+{
+	$messages = $dbManager->GetLastMessages($chatRoom->id, NB_MESSAGES_TO_KEEP);
+	header('Content-Type: application/json');
+	echo '{"dateLastGetMessages": ',$time,', "chatLines": ',json_encode($messages),', "nbIps": ', count($chatRoom->users),'}';
+	exit;
+}
+else
+{
+	// Update the amount of users online to the browser
+    header('Content-Type: application/json');
+	echo '{ "nbIps": ', count($chatRoom->users),' }';
+	exit;
+}
+
+echo "noRoom";
