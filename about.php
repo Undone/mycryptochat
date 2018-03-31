86257cb1317a2953ae0ab09caf3147b41e366247
Undone <undonecoding@outlook.com>
2018-03-31 19:20:49 +0300

    removed unused modernizr js library

--- about.php
+++ about.php
@@ -9,9 +9,6 @@
         <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
         <meta name="viewport" content="width=device-width" />
         <link href="styles/myCryptoChat.css" rel="stylesheet"/>
-
-        <script src="scripts/modernizr.js"></script>
-
     </head>
     <body>
         <header>
