7cf1c8198be311ce07a5de013b05b3ef824524c9
Undone <undonecoding@outlook.com>
2018-04-04 19:24:10 +0300

    Code cleanup

--- newroom.php
+++ newroom.php
@@ -19,32 +19,24 @@ $selfDestroys = isset($_POST['selfDestroys']) && $_POST['selfDestroys'] == 'true
 $isRemovable = isset($_POST['isRemovable']) && $_POST['isRemovable'] == 'true';
 $removePassword = $_POST['removePassword'];
 
-$userHash = getHashForIp();
-
 // we generate a random key
 $key = randomString(20);
 
 // we create the chat room object
-$chatRoom = new ChatRoom();
-$chatRoom->id = $key;
-$chatRoom->dateCreation = $time;
-$chatRoom->dateLastNewMessage = $time;
-$chatRoom->dateEnd = $nbMinutesToLive != 0 ? $time + ($nbMinutesToLive * 60) : 0;
+$chatRoom 						= new ChatRoom($key);
+$chatRoom->dateCreation 		= $time;
+$chatRoom->dateLastNewMessage 	= $time;
+$chatRoom->dateEnd 				= $nbMinutesToLive != 0 ? $time + ($nbMinutesToLive * 60) : 0;
 $chatRoom->noMoreThanOneVisitor = $selfDestroys;
-$chatRoom->isRemovable = $isRemovable;
-$chatRoom->removePassword = $removePassword;
-$chatRoom->userId = $userHash;
-
-$chatUser = array();
-$chatUser['id'] = $userHash;
-$chatUser['dateLastSeen'] = $time;
-
-$chatRoom->addUser($chatUser);
+$chatRoom->isRemovable 			= $isRemovable;
+$chatRoom->removePassword 		= $removePassword;
 
 $dbManager = new DbManager();
+
 // we delete old chatrooms
-$dbManager->CleanChatrooms($time);
+$dbManager->cleanChatrooms();
+
 // we save the chat room in the database
-$dbManager->CreateChatroom($chatRoom);
+$dbManager->createChatroom($chatRoom);
 
 header('Location: chatroom.php?id=' . $key);
